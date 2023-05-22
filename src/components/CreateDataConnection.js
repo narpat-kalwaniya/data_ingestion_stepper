a5b1030fc466f57e5448f8f67712a2161be86f98
rajesh <rajeshkumar.j@tigeranalytics.com>
2023-05-22 11:23:42 +0530

    implemented enable disable property in Create Data Collection and Source Entity Selection

--- src/components/CreateDataConnection.js
+++ src/components/CreateDataConnection.js
@@ -5,6 +5,13 @@ import * as Icon from "react-bootstrap-icons";
 // import "./Page1.css";
 
 const DefineDataConnection = () => {
+  const [dataSourceType, setDataSourceType] = useState("");
+
+  const selectChangeHandler = (event) => {
+    const { value } = event.target;
+    setDataSourceType(value);
+    // console.log(value);
+  };
   return (
     <div className="page1">
       <Row>
@@ -12,60 +19,65 @@ const DefineDataConnection = () => {
           <div className="text-left">
             <Form>
               <Row>
-                  <div className="form-group">
-                    <Form.Label>Data Source Type</Form.Label>
-                    <Form.Select aria-label="" disabled={false}>
-                      <option>{""}</option>
-                      <option value={"RDBMS"}>RDBMS</option>
-                      <option value={"FILE"}>FILE</option>
-                    </Form.Select>
-                  </div>
-                  <div>
-                    <Row>
-                      <Form.Label>Data Source Connection</Form.Label>
+                <div className="form-group">
+                  <Form.Label>Data Source Type</Form.Label>
+                  <Form.Select
+                    aria-label=""
+                    value={dataSourceType}
+                    onChange={selectChangeHandler}
+                    disabled={false}
+                  >
+                    <option>{""}</option>
+                    <option value={"RDBMS"}>RDBMS</option>
+                    <option value={"FILE"}>FILE</option>
+                  </Form.Select>
+                </div>
+                <div>
+                  <Row>
+                    <Form.Label>Data Source Connection</Form.Label>
 
-                      <Col>
-                        <Form.Select>
-                          <option>{""}</option>
-                          <option>dsc1</option>
-                          <option>dsc2</option>
-                        </Form.Select>
-                      </Col>
-                      <Col>
-                        <Icon.CloudPlusFill size={40} className="icon"/>
-                      </Col>
-                    </Row>
-                  </div>
-                  <div>
-                    <Row>
-                      <Form.Label>Data Target Connection</Form.Label>
-                      <Col>
-                        <Form.Select>
-                          <option>{""}</option>
-                          <option>dtc1</option>
-                          <option>dtc2</option>
-                        </Form.Select>
-                      </Col>
-                      <Col>
-                        <Icon.PatchPlusFill size={40} className="icon"/>
-                      </Col>
-                    </Row>
-                  </div>
-                  <div>
-                    <Row>
-                      <Form.Label>Application</Form.Label>
-                      <Col>
-                        <Form.Select>
-                          <option>{""}</option>
-                          <option>dtc1</option>
-                          <option>dtc2</option>
-                        </Form.Select>
-                      </Col>
-                      <Col>
-                      <Icon.WindowPlus className="icon" size = {40}/>
-                      </Col>
-                    </Row>
-                  </div>
+                    <Col>
+                      <Form.Select disabled={dataSourceType !== "RDBMS"}>
+                        <option>{""}</option>
+                        <option>dsc1</option>
+                        <option>dsc2</option>
+                      </Form.Select>
+                    </Col>
+                    <Col>
+                      <Icon.CloudPlusFill size={40} className="icon" />
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row>
+                    <Form.Label>Data Target Connection</Form.Label>
+                    <Col>
+                      <Form.Select disabled={dataSourceType !== "RDBMS"}>
+                        <option>{""}</option>
+                        <option>dtc1</option>
+                        <option>dtc2</option>
+                      </Form.Select>
+                    </Col>
+                    <Col>
+                      <Icon.PatchPlusFill size={40} className="icon" />
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row>
+                    <Form.Label>Application</Form.Label>
+                    <Col>
+                      <Form.Select disabled={dataSourceType !== "RDBMS"}>
+                        <option>{""}</option>
+                        <option>dtc1</option>
+                        <option>dtc2</option>
+                      </Form.Select>
+                    </Col>
+                    <Col>
+                      <Icon.WindowPlus className="icon" size={40} />
+                    </Col>
+                  </Row>
+                </div>
                 <Col></Col>
               </Row>
             </Form>
--- src/components/SourceEntitySelection.js
+++ src/components/SourceEntitySelection.js
@@ -1,50 +1,162 @@
+// import React, { useState } from "react";
+// import { Container, Form, Row, Col, Card, Button } from "react-bootstrap";
+
+// const SourceEntitySelection = () => {
+//   return (
+//     <div className="page1">
+//       <Row>
+//         <Card.Body>
+//           <div className="text-left">
+//             <Form>
+//               <Row>
+//                 <Col sm={6}>
+//                   <div className="form-group">
+//                     <Form.Label>Source Type</Form.Label>
+//                     <Form.Select aria-label="" disabled={false}>
+//                       <option>{""}</option>
+//                       <option>Query</option>
+//                       <option>Table</option>
+//                     </Form.Select>
+//                   </div>
+//                   <div>
+//                     <Form.Label>Query</Form.Label>
+//                     <Form.Control></Form.Control>
+//                   </div>
+//                 </Col>
+//                 <Col>
+//                   <div>
+//                     <Form.Label>Database Name</Form.Label>
+//                     <Form.Control></Form.Control>
+//                   </div>
+//                   <div>
+//                     <Form.Label>Schema Name</Form.Label>
+//                     <Form.Control></Form.Control>
+//                   </div>
+//                   <div>
+//                     <Form.Label>Table Name</Form.Label>
+//                     <Form.Control></Form.Control>
+//                   </div>
+//                   <div>
+//                     <Form.Label>Bucket Name</Form.Label>
+//                     <Form.Control></Form.Control>
+//                   </div>
+//                   <div>
+//                     <Form.Label>Full File Name</Form.Label>
+//                     <Form.Control></Form.Control>
+//                   </div>
+//                 </Col>
+//               </Row>
+//             </Form>
+//           </div>
+//         </Card.Body>
+//       </Row>
+//     </div>
+//   );
+// };
+
+// export default SourceEntitySelection;
+
 import React, { useState } from "react";
 import { Container, Form, Row, Col, Card, Button } from "react-bootstrap";
 
+// import "./SourceEntitySelection.css";
+
 const SourceEntitySelection = () => {
+  const [sourceType, setSourceType] = useState("");
+
+  const selectChangeHandler = (event) => {
+    const { value } = event.target;
+    setSourceType(value);
+  };
+
   return (
     <div className="page1">
       <Row>
         <Card.Body>
           <div className="text-left">
             <Form>
-              <Row>
+              <Row className="mb-3">
                 <Col sm={6}>
                   <div className="form-group">
                     <Form.Label>Source Type</Form.Label>
-                    <Form.Select aria-label="" disabled={false}>
-                      <option>{""}</option>
-                      <option>Query</option>
-                      <option>Table</option>
+                    <Form.Select
+                      aria-label=""
+                      onChange={selectChangeHandler}
+                      value={sourceType}
+                      disabled={false}
+                    >
+                      <option>{"Select"}</option>
+                      <option value={"Query"}>Query</option>
+                      <option value={"Table"}>Table</option>
                     </Form.Select>
                   </div>
-                  <div>
-                    <Form.Label>Query</Form.Label>
-                    <Form.Control></Form.Control>
+                </Col>
+              </Row>
+              <Row className="mb-3">
+                <div>
+                  <Form.Label>Query</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    className="textbox1"
+                    disabled={sourceType !== "Query"}
+                  />
+                </div>
+              </Row>
+              <Row className="mb-3">
+                <Col>
+                  <div className="form-group">
+                    <Form.Label>Database Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      className="textbox1"
+                      disabled={sourceType !== "Table"}
+                    />
                   </div>
                 </Col>
                 <Col>
-                  <div>
-                    <Form.Label>Database Name</Form.Label>
-                    <Form.Control></Form.Control>
-                  </div>
-                  <div>
+                  <div className="form-group">
                     <Form.Label>Schema Name</Form.Label>
-                    <Form.Control></Form.Control>
-                  </div>
-                  <div>
-                    <Form.Label>Table Name</Form.Label>
-                    <Form.Control></Form.Control>
-                  </div>
-                  <div>
-                    <Form.Label>Bucket Name</Form.Label>
-                    <Form.Control></Form.Control>
-                  </div>
-                  <div>
-                    <Form.Label>Full File Name</Form.Label>
-                    <Form.Control></Form.Control>
+                    <Form.Control
+                      type="text"
+                      className="textbox1"
+                      disabled={sourceType !== "Table"}
+                    />
                   </div>
                 </Col>
+                <Col>
+                  <div className="form-group">
+                    <Form.Label>Table Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      className="textbox1"
+                      disabled={sourceType !== "Table"}
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="form-group">
+                    <Form.Label>Bucket Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      className="textbox1"
+                      disabled={false}
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="form-group">
+                    <Form.Label>Full File Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      className="textbox1"
+                      disabled={false}
+                    />
+                  </div>
+                </Col>
+                <Col></Col>
               </Row>
             </Form>
           </div>
