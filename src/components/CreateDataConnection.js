704552c9c55678aad2baae71e973a916d874f76a
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-05-26 15:36:29 +0530

    integrating the api in first step

--- src/components/CreateDataConnection.js
+++ src/components/CreateDataConnection.js
@@ -1,18 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Form, Row, Col, Card, Button } from "react-bootstrap";
 import * as Icon from "react-bootstrap-icons";
 
 // import "./Page1.css";
 
 const DefineDataConnection = (props) => {
-  const pageAnswers = useState({
-    page: props.step,
-    formdata: {
-      "Data Source Connection": "",
-      "Data Target Connection": "",
-      "Application": "",
-    },
-  });
+  const [connections, setConnections] = useState([]);
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    const fetchConnections = async () => {
+      try {
+        const response = await fetch(
+          "http://ec2-54-197-121-247.compute-1.amazonaws.com:8000/conndetails/"
+        );
+        const data = await response.json();
+        setConnections(data);
+      } catch (error) {
+        console.error("Error fetching connections:", error);
+      }
+    };
+
+    const fetchApplications = async () => {
+      try {
+        const response = await fetch(
+          "http://ec2-54-197-121-247.compute-1.amazonaws.com:8000/appdetails/"
+        );
+        const data = await response.json();
+        setApplications(data);
+      } catch (error) {
+        console.error("Error fetching applications:", error);
+      }
+    };
+
+    fetchConnections();
+    fetchApplications();
+  }, []);
+
+  const handleSelection = (event) => {
+    const selectedConnectionName = event.target.value;
+    // Handle the selected connection name here
+    console.log(selectedConnectionName);
+  };
+
+  const filteredSourceConnections = connections.filter(
+    (connection) => connection.connection_type === "POSTGRES"
+  );
+
+  const filteredTargetConnections = connections.filter(
+    (connection) => connection.connection_type === "SNOWFLAKE"
+  );
+  console.log("data is", connections.connection_name);
+
+  // const pageAnswers = useState({
+  //   page: props.step,
+  //   formdata: {
+  //     "Data Source Connection": "",
+  //     "Data Target Connection": "",
+  //     Application: "",
+  //   },
+  // });
 
   return (
     <div className="page1">
@@ -34,10 +81,15 @@ const DefineDataConnection = (props) => {
                     <Form.Label>Data Source Connection</Form.Label>
 
                     <Col>
-                      <Form.Select>
-                        <option>{""}</option>
-                        <option>dsc1</option>
-                        <option>dsc2</option>
+                      <Form.Select onChange={handleSelection}>
+                        {filteredSourceConnections.map((connection) => (
+                          <option
+                            key={connection.connection_id}
+                            value={connection.connection_name}
+                          >
+                            {connection.connection_name}
+                          </option>
+                        ))}
                       </Form.Select>
                     </Col>
                     <Col>
@@ -50,9 +102,14 @@ const DefineDataConnection = (props) => {
                     <Form.Label>Data Target Connection</Form.Label>
                     <Col>
                       <Form.Select>
-                        <option>{""}</option>
-                        <option>dtc1</option>
-                        <option>dtc2</option>
+                        {filteredTargetConnections.map((connection) => (
+                          <option
+                            key={connection.connection_id}
+                            value={connection.connection_name}
+                          >
+                            {connection.connection_name}
+                          </option>
+                        ))}
                       </Form.Select>
                     </Col>
                     <Col>
@@ -65,9 +122,14 @@ const DefineDataConnection = (props) => {
                     <Form.Label>Application</Form.Label>
                     <Col>
                       <Form.Select>
-                        <option>{""}</option>
-                        <option>dtc1</option>
-                        <option>dtc2</option>
+                        {applications.map((application) => (
+                          <option
+                            key={application.app_id}
+                            value={application.app_name}
+                          >
+                            {application.app_name}
+                          </option>
+                        ))}
                       </Form.Select>
                     </Col>
                     <Col>
