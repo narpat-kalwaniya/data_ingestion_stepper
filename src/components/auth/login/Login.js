c5dc3e348c264c7600827cfd8b912cdbee30459c
Narpat Kalwaniya <narpat.kalwaniya@tigeranalytics.local>
2023-06-12 18:21:54 +0530

    review page redesigned

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -1,21 +1,25 @@
 import React, { useEffect } from "react";
 // import "./login.css";
+import classes from "./Login.module.css";
+import "../../../styles/main.css";
 
 import log4 from "./undraw_data_processing_yrrv.svg";
 import tigerImage from "./download (2).png";
 import snowflakeImage from "./download (1).png";
+import googleLogo from "./google-logo.png";
 
 import { Helmet } from "react-helmet";
 
 import { signInWithGoogle } from "../../../services/firebase";
+import { Button } from "react-bootstrap";
 
 const LoginPage = (props) => {
-  const loginHandler = () => {
-    props.setIsLoggedIn(!props.isLoggedIn);
-  };
+  // const loginHandler = () => {
+  //   props.setIsLoggedIn(!props.isLoggedIn);
+  // };
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://kit.fontawesome.com/64d58efce2.js";
+    // script.src = "https://kit.fontawesome.com/64d58efce2.js";
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
 
@@ -24,20 +28,20 @@ const LoginPage = (props) => {
     };
   }, []);
   return (
-    <div className="container">
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" class="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
+    <div className={classes.container}>
+      <div className={classes["forms-container"]}>
+        <div className={classes["signin-signup"]}>
+          <form action="#" className={classes["sign-in-form"]}>
+            <h2 className={classes.title}>Sign in</h2>
+            <div className={classes["input-field"]}>
               <i className="fas fa-user"></i>
               <input type="email" placeholder="Email ID" />
             </div>
-            <div className="input-field">
+            <div className={classes["input-field"]}>
               <i className="fas fa-lock"></i>
               <input type="password" placeholder="Password" />
             </div>
-            <div className="forget">
+            <div className={classes.forget}>
               <label for="">
                 <input type="checkbox"></input>
                 Remember Me
@@ -47,43 +51,63 @@ const LoginPage = (props) => {
             <input
               type="submit"
               value="Login"
-              className="btn solid"
-              onClick={loginHandler}
+              // className="btn solid"
+              className="btn-s-1"
             />
-            <p className="social-text">Or </p>
-            <div>
-              <input
+            <p className={classes["social-text"]}>Or </p>
+            <div
+              class="btn-s-1"
+              style={{ display: "flex", alignItems: "center" }}
+              onClick={signInWithGoogle}
+            >
+              <div style={{ margin: "0px", alignItems: "left" }}>
+                <img
+                  style={{
+                    width: "40px",
+                    padding: "10px",
+                    backgroundColor: "white",
+                    borderRadius: "50%",
+                  }}
+                  alt="Google sign-in"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+                />
+              </div>
+              Login with Google
+              {/* <input
                 type="submit"
                 value="Sign in with Google"
-                className="google-signin"
+                className="btn-s-1"
                 onClick={signInWithGoogle}
-              />
+              /> */}
             </div>
           </form>
         </div>
       </div>
 
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
-            <div></div>
-            <div className="logo">
-              <img src={tigerImage} className="tiger-image" alt="Logo" />
-              <div className="vertical-line" />
+      <div className={classes["panels-container"]}>
+        <div className={`${classes.panel} ${classes["left-panel"]}`}>
+          <div className={classes.content}>
+            <div className={classes.logo}>
+              <img
+                src={tigerImage}
+                className={classes["tiger-image"]}
+                alt="Logo"
+              />
+              <div className={classes["vertical-line"]} />
               <img
                 src={snowflakeImage}
-                className="snowflake-image"
+                className={classes["snowflake-image"]}
                 alt="Logo"
               />
             </div>
-            <h2 className="heading2">Snowflake Data Fabric</h2>
+            <h2 className={classes.heading2}>Snowflake Data Fabric</h2>
           </div>
-          <img src={log4} className="image" alt="" />
+          <img src={log4} className={classes.image} alt="" />
         </div>
       </div>
       <Helmet>
         <script
-          src="https://kit.fontawesome.com/64d58efce2.js"
+          // src="https://kit.fontawesome.com/64d58efce2.js"
           crossorigin="anonymous"
         ></script>
       </Helmet>
