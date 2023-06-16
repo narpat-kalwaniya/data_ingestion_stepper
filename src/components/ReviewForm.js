6c10aac2a701ef9b8bdbef9899f1dae2ecb87963
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-06-16 15:21:17 +0530

    added some extra keys in schedule

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -117,9 +117,9 @@ const ReviewFrom = (props) => {
     }
   };
 
-  console.log("final ingestion data", safeStringify(ingestionData[0]));
+  // console.log("final ingestion data", safeStringify(ingestionData[0]));
   console.log("final ingestion data without stringy", ingestionData);
-  console.log(props.formData);
+  // console.log(props.formData);
 
   const renderTable = (data) => {
     return (
