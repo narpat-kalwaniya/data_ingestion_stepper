2b594b6ce896c2c14f77f5c1d12639378787a457
Narpat Kalwaniya <narpat.kalwaniya@tigeranalytics.local>
2023-06-20 18:22:35 +0530

    latest commit 20/06

--- src/components/DefineDataValidation.js
+++ src/components/DefineDataValidation.js
@@ -1,38 +1,143 @@
+// import React, { useEffect, useState } from "react";
+// import { Table, Form, FormControl } from "react-bootstrap";
+// import Select from "react-select";
+// import TagsInput from "react-tagsinput";
+// import "react-tagsinput/react-tagsinput.css";
+
+// const headers = [
+//   "Column Name",
+//   "Target Data Type",
+//   "Validation Rule",
+//   "Validation Input",
+//   "Quality Score",
+// ];
+
+// export const DefineDataValidation = ({ formData }) => {
+//   const [testcases, setTestcases] = useState([]);
+//   const [selectedTestcases, setSelectedTestcases] = useState([]);
+//   const [tagValues, setTagValues] = useState([]);
+
+//   useEffect(() => {
+//     fetchTestcases();
+//   }, []);
+
+//   const fetchTestcases = async () => {
+//     try {
+//       const response = await fetch(
+//         "http://ec2-54-197-121-247.compute-1.amazonaws.com:8000/testcasemaster/"
+//       );
+//       const data = await response.json();
+//       setTestcases(data);
+//       setSelectedTestcases(Array(formData.tableData.length).fill([]));
+//     } catch (error) {
+//       console.log("Error fetching test cases:", error);
+//     }
+//   };
+
+//   const handleTestcaseChange = (option, index) => {
+//     setSelectedTestcases((prevSelectedTestcases) => {
+//       const updatedSelectedTestcases = [...prevSelectedTestcases];
+//       updatedSelectedTestcases[index] = option;
+//       return updatedSelectedTestcases;
+//     });
+//   };
+
+//   const handleTagChange = (tags, index) => {
+//     setTagValues((prevTagValues) => {
+//       const updatedTagValues = [...prevTagValues];
+//       updatedTagValues[index] = tags;
+//       return updatedTagValues;
+//     });
+//   };
+
+//   const validateTagCount = (tags) => {
+//     const desiredCount = 3; // Specify the desired tag count here
+//     if (tags.length > desiredCount) {
+//       return false;
+//     }
+//     return true;
+//   };
+
+//   return (
+//     <Table responsive>
+//       <thead>
+//         <tr>
+//           {headers.map((name, index) => (
+//             <th key={index}>{name}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {formData.tableData.map((column, index) => (
+//           <tr key={index}>
+//             <td>{column.column_name}</td>
+//             <td>{column.target_datatype}</td>
+//             <td>
+//               <Select
+//                 value={selectedTestcases[index]}
+//                 onChange={(option) => handleTestcaseChange(option, index)}
+//                 options={testcases.map((testcase) => ({
+//                   value: testcase.testcase_name,
+//                   label: testcase.testcase_name_alias,
+//                 }))}
+//                 placeholder="Select Test Case"
+//                 isSearchable
+//               />
+//             </td>
+//             <td>
+//               <Form.Group>
+//                 <TagsInput
+//                   value={tagValues[index] || []}
+//                   onChange={(tags) => handleTagChange(tags, index)}
+//                   validationRegex={/^.*$/}
+//                   validationError="Please enter valid tags"
+//                   tagProps={{ className: "react-tagsinput-tag info" }}
+//                 />
+//                 {!validateTagCount(tagValues[index] || []) && (
+//                   <Form.Text className="text-danger">
+//                     Please select fewer than 3 values
+//                   </Form.Text>
+//                 )}
+//               </Form.Group>
+//             </td>
+//             <td>
+//               <FormControl
+//                 type="text"
+//                 onChange={(e) => {
+//                   // Handle Quality Score change
+//                 }}
+//               />
+//             </td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </Table>
+//   );
+// };
+
 import React, { useEffect, useState } from "react";
-import {
-  Table,
-  Form,
-  Input,
-  InputGroup,
-  Col,
-  Row,
-  Badge,
-  FormControl,
-} from "react-bootstrap";
-
+import { Table, Form, FormControl, Button } from "react-bootstrap";
 import Select from "react-select";
-
-import {
-  Select as AntdSelect,
-  Form as AntdForm,
-  Input as AntdInput,
-  Tag,
-} from "antd";
+import TagsInput from "react-tagsinput";
+import "react-tagsinput/react-tagsinput.css";
+import { HiOutlinePlus } from "react-icons/hi";
 
 const headers = [
   "Column Name",
-  // "Source Data Type",
   "Target Data Type",
   "Validation Rule",
   "Validation Input",
+  "Add Test Cases",
   "Quality Score",
 ];
 
-export const DefineDataValidation = ({ formData, updateFormData }) => {
+export const DefineDataValidation = ({ formData }) => {
   const [testcases, setTestcases] = useState([]);
   const [selectedTestcases, setSelectedTestcases] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [tableData, setTableData] = useState([...formData.tableData]);
+  const [tagValues, setTagValues] = useState([]);
+  const [inputCounts, setInputCounts] = useState(
+    formData.tableData.map(() => 1)
+  );
 
   useEffect(() => {
     fetchTestcases();
@@ -42,168 +147,138 @@ export const DefineDataValidation = ({ formData, updateFormData }) => {
     try {
       const response = await fetch(
         "http://ec2-54-197-121-247.compute-1.amazonaws.com:8000/testcasemaster/"
-      ); // Replace with your API endpoint
+      );
       const data = await response.json();
       setTestcases(data);
-      setSelectedTestcases(Array(formData.tableData.length).fill(""));
+      // setSelectedTestcases(Array(formData.tableData.length).fill([]));
     } catch (error) {
       console.log("Error fetching test cases:", error);
     }
   };
 
-  const targetDataTypes = [
-    "ARRAY",
-    "BIGINT",
-    "BINARY",
-    "BOOLEAN",
-    "CHAR",
-    "DECIMAL",
-    "DOUBLE",
-    "DOUBLE PRECISION",
-    "INT",
-    "INTEGER",
-    "NCHAR",
-    "NUMERIC",
-    "NVARCHAR",
-    "NUMBER",
-    "STRING",
-    "TEXT",
-    "TIME",
-    "TIMASTAMP_TZ",
-    "TIMESTAMP_LTZ",
-    "TIMESTAMP_NTZ",
-    "VARBINARY",
-    "VARCHAR",
-  ];
+  const handleAddInput = (index) => {
+    setInputCounts((prevCounts) => {
+      const updatedCounts = [...prevCounts];
+      updatedCounts[index] += 1;
+      return updatedCounts;
+    });
+  };
 
-  // const handleTestcaseChange = (e, index) => {
-  //   const updatedTestcases = [...selectedTestcases];
-  //   updatedTestcases[index] = e.target.value;
-  //   setSelectedTestcases(updatedTestcases);
-  // };
-
-  const validationRuleHandler = (option, index) => {
+  const handleTestcaseChange = (option, rowIndex, inputIndex) => {
     setSelectedTestcases((prevSelectedTestcases) => {
       const updatedSelectedTestcases = [...prevSelectedTestcases];
-      updatedSelectedTestcases[index] = option;
+      const selectedTestcasesForRow = updatedSelectedTestcases[rowIndex] || [];
+      selectedTestcasesForRow[inputIndex] = option;
+      updatedSelectedTestcases[rowIndex] = selectedTestcasesForRow;
       return updatedSelectedTestcases;
     });
-    const updatedTableData = [...tableData];
-    updatedTableData[index].validation_rule = option.value;
-    setTableData(updatedTableData);
-
-    const updatedFormData = {
-      ...formData,
-      tableData: updatedTableData,
-    };
-    updateFormData(updatedFormData);
   };
 
-  const validationInputHandler = (target, index) => {
-    const updatedTableData = [...tableData];
-    updatedTableData[index].validation_input = target.value;
-    setTableData(updatedTableData);
-
-    const updatedFormData = {
-      ...formData,
-      tableData: updatedTableData,
-    };
-    updateFormData(updatedFormData);
+  const handleTagChange = (tags, rowIndex, inputIndex) => {
+    setTagValues((prevTagValues) => {
+      const updatedTagValues = [...prevTagValues];
+      const tagValuesForRow = updatedTagValues[rowIndex] || [];
+      tagValuesForRow[inputIndex] = tags;
+      updatedTagValues[rowIndex] = tagValuesForRow;
+      return updatedTagValues;
+    });
   };
 
-  const validateTagCount = (_, value) => {
+  const validateTagCount = (tags) => {
     const desiredCount = 3; // Specify the desired tag count here
-    const tagCount = value ? value.length : 0;
-    if (tagCount > desiredCount) {
-      return Promise.reject(`Please select fewer than ${desiredCount} values`);
-    } else {
-      return Promise.resolve();
+    if (tags.length > desiredCount) {
+      return false;
     }
+    return true;
   };
-
-  const qualityScoreHandler = (value, index) => {
-    const updatedTableData = [...tableData];
-    updatedTableData[index].quality_score = value;
-    setTableData(updatedTableData);
-  };
-
-  console.log(formData.tableData);
+  console.log("testcases", selectedTestcases);
+  console.log("tags", tagValues);
 
   return (
     <Table responsive>
-      <thead
-        style={{
-          backgroundColor: "#F3F3F3",
-          fontSize: "12px",
-          height: "50px",
-          alignItems: "center",
-        }}
-      >
+      <thead>
         <tr>
           {headers.map((name, index) => (
             <th key={index}>{name}</th>
           ))}
         </tr>
       </thead>
-      <tbody style={{ fontSize: "12px" }}>
-        {formData.tableData.map((column, index) => (
-          <tr
-            key={index}
-            style={{
-              height: "20px",
-            }}
-          >
+      <tbody>
+        {formData.tableData.map((column, rowIndex) => (
+          <tr key={rowIndex}>
             <td>{column.column_name}</td>
-            {/* <td>{column.data_type}</td> */}
             <td>{column.target_datatype}</td>
             <td>
-              {/* <Form.Control
-                as="select"
-                value={selectedTestcases[index]}
-                onChange={(e) => handleTestcaseChange(e, index)}
-                placeholder="Enter expectation"
-              >
-                <option value="">Select Test Case</option>
-                {testcases.map((testcase) => (
-                  <option
-                    key={testcase.testcase_master_id}
-                    value={testcase.testcase_name}
-                  >
-                    {testcase.testcase_name_alias}
-                  </option>
+              {Array(inputCounts[rowIndex])
+                .fill()
+                .map((_, inputIndex) => (
+                  <React.Fragment key={`${rowIndex}-${inputIndex}`}>
+                    <Select
+                      value={
+                        selectedTestcases[rowIndex] &&
+                        selectedTestcases[rowIndex][inputIndex]
+                      }
+                      onChange={(option) =>
+                        handleTestcaseChange(option, rowIndex, inputIndex)
+                      }
+                      options={testcases.map((testcase) => ({
+                        value: testcase.testcase_name,
+                        label: testcase.testcase_name_alias,
+                      }))}
+                      placeholder="Select Test Case"
+                      isSearchable
+                    />
+                    <br /> {/* Add spacing between the fields */}
+                  </React.Fragment>
                 ))}
-              </Form.Control> */}
-              <Select
-                value={selectedTestcases[index]}
-                onChange={(option) => validationRuleHandler(option, index)}
-                options={testcases.map((testcase) => ({
-                  value: testcase.testcase_name,
-                  label: testcase.testcase_name_alias,
-                }))}
-                placeholder="Select Test Case"
-                isSearchable
-                className="custom-select custom-style"
-              />
             </td>
             <td>
-              {/* <AntdForm.Item
-                name={`expectationInput-${index}`}
-                rules={[
-                  {
-                    validator: validateTagCount,
-                  },
-                ]}
-                onChange={(e) => validationInputHandler(e.target, index)}
-                className="custom-select custom-style"
+              {Array(inputCounts[rowIndex])
+                .fill()
+                .map((_, inputIndex) => (
+                  <React.Fragment key={`${rowIndex}-${inputIndex}`}>
+                    <Form.Group>
+                      <TagsInput
+                        value={
+                          (tagValues[rowIndex] &&
+                            tagValues[rowIndex][inputIndex]) ||
+                          []
+                        }
+                        onChange={(tags) =>
+                          handleTagChange(tags, rowIndex, inputIndex)
+                        }
+                        validationRegex={/^.*$/}
+                        validationError="Please enter valid tags"
+                        tagProps={{ className: "react-tagsinput-tag info" }}
+                      />
+                      {!validateTagCount(
+                        (tagValues[rowIndex] &&
+                          tagValues[rowIndex][inputIndex]) ||
+                          []
+                      ) && (
+                        <Form.Text className="text-danger">
+                          Please select fewer than 3 values
+                        </Form.Text>
+                      )}
+                    </Form.Group>
+                    <br /> {/* Add spacing between the fields */}
+                  </React.Fragment>
+                ))}
+            </td>
+            <td>
+              <Button
+                variant="secondary"
+                onClick={() => handleAddInput(rowIndex)}
               >
-                <AntdSelect mode="tags" placeholder="Expectation Input" />
-              </AntdForm.Item> */}
+                <HiOutlinePlus />
+              </Button>
             </td>
             <td>
-              <Form.Control
+              <FormControl
                 type="text"
-                onChange={(e) => qualityScoreHandler(e.target.value, index)}
-                className="custom-select custom-style"
+                onChange={(e) => {
+                  // Handle Quality Score change
+                }}
               />
             </td>
           </tr>
