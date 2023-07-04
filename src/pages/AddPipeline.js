dfec49aef899927715a710fe2bb5d3d92ea7e1e1
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-07-04 13:48:28 +0530

    add the incremntal column to the backend with data persistence

--- src/pages/AddPipeline.js
+++ src/pages/AddPipeline.js
@@ -37,6 +37,7 @@ function AddPipeline() {
       source_entity_type: "",
       is_select_distinct: false,
       incremental_by: "",
+      source_incremental_column: null,
       incremental_start_time: "",
       incremental_end_time: "",
       incremental_start_sequence: "",
@@ -432,7 +433,7 @@ function AddPipeline() {
                         style={{
                           minHeight: "60vh",
                           maxHeight: "60vh",
-                           overflowY: "scroll",
+                          overflowY: "scroll",
                         }}
                       >
                         <Stepper
