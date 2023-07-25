c0d898c14a540d971fe40991be46a1a776fbd13f
Dashrath Patidar <dashrath.patidar@tigeranalytics.local>
2023-07-25 20:18:12 +0530

    fixed error

--- src/pages/AddPipeline.js
+++ src/pages/AddPipeline.js
@@ -307,12 +307,12 @@ function AddPipeline() {
   const validateInputs6 = () => {
     const newErrors6 = {};
 
-    // if (
-    //   !formData.targetLoadDetails.target_entity_name ||
-    //   formData.targetLoadDetails.target_entity_name === ""
-    // ) {
-    //   newErrors6.target_entity_name = "This field is required";
-    // }
+    if (
+      !formData.targetLoadDetails.target_entity_name ||
+      formData.targetLoadDetails.target_entity_name === ""
+    ) {
+      newErrors6.target_entity_name = "This field is required";
+    }
     if (
       !formData.targetLoadDetails.target_load_type ||
       formData.targetLoadDetails.target_load_type === ""
@@ -327,12 +327,12 @@ function AddPipeline() {
       newErrors6.selectedTableSchema = "This field is required";
     }
 
-    if (
-      !formData.tableData[0]?.target_table ||
-      formData.tableData[0]?.target_table === ""
-    ) {
-      newErrors6.target_table = "This field is required";
-    }
+    // if (
+    //   !formData.tableData[0]?.target_table ||
+    //   formData.tableData[0]?.target_table === ""
+    // ) {
+    //   newErrors6.target_table = "This field is required";
+    // }
 
     // if (
     //   !formData.tableData[0]?.target_database ||
