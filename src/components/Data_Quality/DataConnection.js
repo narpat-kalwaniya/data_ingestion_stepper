28a3f4842c9f9682f64ee1ee2e303b59eae20e2d
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-08-23 17:37:27 +0530

    added data quality stepper

---
+++ src/components/Data_Quality/DataConnection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DataConnection = () => {
+  return <div>Data conneciton</div>;
+};
+
+export default DataConnection;
---
+++ src/components/Data_Quality/SectionMenuDataQuality.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  Form,
+  Modal,
+  ListGroup,
+} from "react-bootstrap";
+
+import "../Migration/AppMig.css";
+
+const sections = [
+  "Data Connection",
+  "Source Entity",
+  "Target Schema",
+  "Configure Rules",
+];
+
+const SectionMenuDataQuality = (props) => {
+  const handleHover = () => {
+    // Handle hover event
+  };
+  return (
+    <div style={{ width: "214px", padding: "0px" }}>
+      <Col>
+        <ListGroup>
+          {sections.map((item, index) => (
+            <ListGroup.Item
+              key={index}
+              style={{
+                height: "100px",
+                backgroundColor: "#FCFCFC",
+                color: index <= props.step - 1 ? "#EA943D" : "darkgray",
+                fontSize: index === props.step - 1 ? "14px" : "13px",
+                fontWeight: index <= props.step - 1 ? 500 : 400,
+                border: "none",
+                borderRadius: "none",
+                transition: "ease-in-out 0.03s",
+              }}
+            >
+              <div className="d-flex align-items-center">
+                <div
+                  className="d-flex align-items-center"
+                  // style={{ position: "relative" }}
+                >
+                  {index > 0 && index < 3 && (
+                    <div
+                      className="mig-vertical-line"
+                      style={{
+                        backgroundColor:
+                          index <= props.step - 1 ? "#EA943D" : "darkgray",
+                        transition: "ease-in-out 0.03s",
+                      }}
+                    />
+                  )}
+                  <div
+                    className="circle"
+                    style={{
+                      borderColor:
+                        index <= props.step - 1 ? "#EA943D" : "darkgray",
+                      backgroundColor:
+                        index < props.step - 1 ? "#EA943D" : "white",
+                      transition: "ease-in-out 0.03s",
+                    }}
+                  ></div>
+                </div>
+                {item}
+              </div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Col>
+    </div>
+  );
+};
+
+export default SectionMenuDataQuality;
