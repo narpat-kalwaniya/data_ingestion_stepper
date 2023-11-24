c4a24a299c34f2caac092a6287ba1be135583161
Uttam Verma <uttam.verma@tigeranalytics.local>
2023-11-24 16:42:23 +0530

    added the two modal

--- src/components/DataObservability/TableHealth/FreshnessModal.js
+++ src/components/DataObservability/TableHealth/FreshnessModal.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Modal, Button, Form, Dropdown } from "react-bootstrap";
+import { Modal, Button, Form, Dropdown, Row, Col } from "react-bootstrap";
 
 const FreshnessModal = ({ show, onClose }) => {
   const [selectedDropdown1, setSelectedDropdown1] = useState("");
   const [selectedDropdown2, setSelectedDropdown2] = useState("");
-  const [frequency, setFrequency] = useState("");
+  const [selectedFreqency, setSelectedFreqency] = useState("");
+  const [frequency, setFrequency] = useState(); // Initialize frequency as a number
 
   const handleSave = () => {
     // Handle saving the selected values and checkboxes here
@@ -17,38 +18,57 @@ const FreshnessModal = ({ show, onClose }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group>
-            <Form.Label>Table Name</Form.Label>
-            <Dropdown onSelect={(value) => setSelectedDropdown1(value)}>
-              <Dropdown.Toggle variant="light">
-                {selectedDropdown1 || "Select an option"}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item eventKey="option1">Option 1</Dropdown.Item>
-                <Dropdown.Item eventKey="option2">Option 2</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Column Name</Form.Label>
-            <Dropdown onSelect={(value) => setSelectedDropdown2(value)}>
-              <Dropdown.Toggle variant="light">
-                {selectedDropdown2 || "Select an option"}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item eventKey="option1">Option 1</Dropdown.Item>
-                <Dropdown.Item eventKey="option2">Option 2</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Form.Group>
-          <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Table Name</Form.Label>
+              <Form.Select
+                value={selectedDropdown1}
+                onChange={(e) => setSelectedDropdown1(e.target.value)}
+                style={{ fontSize: "14px" }}
+              >
+                <option value="">Select an option</option>
+                <option value="option1">table</option>
+                <option value="option2">table2</option>
+                <option value="option3">table3</option>
+              </Form.Select>
+
+              <Form.Label>Column Name</Form.Label>
+              <Form.Select
+                value={selectedDropdown2}
+                onChange={(e) => setSelectedDropdown2(e.target.value)}
+                style={{ fontSize: "14px" }}
+              >
+                <option value="">Select an option</option>
+                <option value="option1">table</option>
+                <option value="option2">table2</option>
+                <option value="option3">table3</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row>
             <Form.Label>Frequency</Form.Label>
-            <Form.Control
-              type="text"
-              value={frequency}
-              onChange={(e) => setFrequency(e.target.value)}
-            />
-          </Form.Group>
+            <Col>
+              <Form.Select
+                value={selectedFreqency}
+                onChange={(e) => setSelectedFreqency(e.target.value)}
+                style={{ fontSize: "14px" }}
+              >
+                <option value="">Select an option</option>
+                <option value="option1">Daily</option>
+                <option value="option2">Weekly</option>
+                <option value="option3">Monthly</option>
+              </Form.Select>
+            </Col>
+            <Col>
+              <Form.Control
+                type="number"
+                value={frequency}
+                onChange={(e) => setFrequency(Number(e.target.value))} // Convert input value to a number
+                step="1"
+                style={{ fontSize: "14px" }}
+              />
+            </Col>
+          </Row>
         </Form>
       </Modal.Body>
       <Modal.Footer>
--- src/components/DataObservability/TableHealth/SchemaModel.js
+++ src/components/DataObservability/TableHealth/SchemaModel.js
@@ -10,43 +10,139 @@ const SchemaModal = ({ show, onClose }) => {
   const [checkbox6, setCheckbox6] = useState(false);
   const [checkbox7, setCheckbox7] = useState(false);
   const [checkbox8, setCheckbox8] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleSave = () => {
     // Handle saving the selected values and checkboxes here
   };
 
   return (
-    <Modal show={show} onHide={onClose}>
+    <Modal show={show} onHide={onClose} size="lg">
       <Modal.Header closeButton>
-        <Modal.Title>My Modal</Modal.Title>
+        <Modal.Title>Table Name</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Row>
-            <Col>
-              <Form.Check>
-                <Form.Check.Input
-                  type="checkbox"
-                  checked={checkbox1}
-                  onChange={() => setCheckbox1(!checkbox1)}
-                />
-                <Form.Check.Label>New Tables</Form.Check.Label>
-              </Form.Check>
-            </Col>
-            <Col>
-              <Form.Check>
-                <Form.Check.Input
-                  type="checkbox"
-                  checked={checkbox2}
-                  onChange={() => setCheckbox2(!checkbox2)}
-                />
-                <Form.Check.Label>Deleted Tables</Form.Check.Label>
-              </Form.Check>
+          <Row style={{ margin: "10px 0" }}>
+            <Col xs={4}>
+              <Form.Label>Table</Form.Label>
+              <Form.Select
+                value={selectedOption}
+                onChange={(e) => setSelectedOption(e.target.value)}
+                style={{ fontSize: "14px" }}
+              >
+                <option value="">Select an option</option>
+                <option value="option1">table</option>
+                <option value="option2">table2</option>
+                <option value="option3">table3</option>
+              </Form.Select>
             </Col>
           </Row>
-          <Row>
+          <Row style={{ margin: "10px 0" }}>
             <Col>
-              <Form.Check>
+              <Form.Label>Table Changes</Form.Label>
+              <div className="radio-group">
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="New Tables"
+                  name="options"
+                  value="RDBMS-TABLE"
+                  checked={checkbox1}
+                  onChange={() => setCheckbox1(!checkbox1)}
+                  className="custom-radio"
+                />
+
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="Deleted Tables"
+                  name="options"
+                  value="RDBMS-QUERY"
+                  checked={checkbox2}
+                  onChange={() => setCheckbox2(!checkbox2)}
+                  className="custom-radio"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row style={{ margin: "10px 0" }}>
+            <Form.Label>Attribute Changes</Form.Label>
+            <Col>
+              <div className="radio-group">
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="New"
+                  name="options"
+                  value="RDBMS-TABLE"
+                  checked={checkbox3}
+                  onChange={() => setCheckbox3(!checkbox3)}
+                  className="custom-radio"
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="radio-group">
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="Modified"
+                  name="options"
+                  value="RDBMS-QUERY"
+                  checked={checkbox4}
+                  onChange={() => setCheckbox4(!checkbox4)}
+                  className="custom-radio"
+                />
+
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="Data Type"
+                  name="options"
+                  value="RDBMS-QUERY"
+                  checked={checkbox5}
+                  onChange={() => setCheckbox5(!checkbox5)}
+                  className="custom-radio"
+                />
+
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="Ordinal Position"
+                  name="options"
+                  value="RDBMS-QUERY"
+                  checked={checkbox6}
+                  onChange={() => setCheckbox6(!checkbox6)}
+                  className="custom-radio"
+                />
+
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="Nullability"
+                  name="options"
+                  value="RDBMS-QUERY"
+                  checked={checkbox7}
+                  onChange={() => setCheckbox7(!checkbox7)}
+                  className="custom-radio"
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="radio-group">
+                <Form.Check
+                  inline
+                  type="checkbox"
+                  label="Deleted"
+                  name="options"
+                  value="RDBMS-QUERY"
+                  checked={checkbox8}
+                  onChange={() => setCheckbox8(!checkbox8)}
+                  className="custom-radio"
+                />
+              </div>
+              {/* <Form.Check>
                 <Form.Check.Input
                   type="checkbox"
                   checked={checkbox3}
@@ -97,7 +193,7 @@ const SchemaModal = ({ show, onClose }) => {
                   onChange={() => setCheckbox8(!checkbox8)}
                 />
                 <Form.Check.Label>Deleted</Form.Check.Label>
-              </Form.Check>
+              </Form.Check> */}
             </Col>
           </Row>
         </Form>
